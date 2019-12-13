2caffdaac9d8ca3daf6c5e056ed9e03db7123c57
beata <beata@emc2wave.com>
2019-12-13 13:36:28 -0800

    const nextbutton

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonElement = document.getElementById('answer-buttons');
